e0d1ef81afa266aebab9604cfc3db04577c00ca1
Noam Raz <noamrazbusiness@gmail.com>
2022-10-11 08:48:02 +0300

    Fixed cursor not being center

--- website2/src/App.js
+++ website2/src/App.js
@@ -14,7 +14,7 @@ const App = () => {
   const [variant, setVariant] = useState("default");
 
 	const cursorX = useMotionValue(-100);
-  	const cursorY = useMotionValue(-100);
+  const cursorY = useMotionValue(-100);
 	const springConfig = { damping: 40, stiffness: 800 };
 	const cursorXSpring = useSpring(cursorX, springConfig);
 	const cursorYSpring = useSpring(cursorY, springConfig);
@@ -28,19 +28,19 @@ const App = () => {
 
   useEffect(() => {
 		const moveCursor = (e) => {
-			cursorX.set(e.clientX - (variants[variant].width / 2));
-      		cursorY.set(e.clientY - (variants[variant].height / 2));
+		  cursorX.set(e.clientX - (variants[variant].width / 2));
+      cursorY.set(e.clientY - (variants[variant].height / 2));
 		}
-
+    
+    window.removeEventListener('mousemove', moveCursor);
 		window.addEventListener('mousemove', moveCursor);
 
 		return () => {
 			window.removeEventListener('mousemove', moveCursor);
 		}
-	}, []);
+	}, [variants]);
 
 	const mouseEnter = (hover="hover") => {
-		console.log(typeof hover);
 		if(typeof hover == 'string')
 			setVariant(hover);
 		else
