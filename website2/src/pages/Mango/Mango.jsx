0be2790e9eacfae0cc2f654ba011c73876e2dddc
Noam Raz <noamrazbusiness@gmail.com>
2023-02-10 01:57:35 +0200

    Updated the mango page

--- website2/src/pages/Mango/Mango.jsx
+++ website2/src/pages/Mango/Mango.jsx
@@ -7,10 +7,12 @@ import "./Mango.css";
 import { useState } from 'react';
 import { useEffect } from 'react';
 
+import YouTube from 'react-youtube'
+
 import ProjectPage from '../../components/ProjectPage/ProjectPage';
 
 import MangoBG from '../../assets/projects/mango/background.webp';
-import InGame from '../../assets/projects/sable/in-game.webp';
+import Banner from '../../assets/projects/mango/banner.webp';
 import HTML5Img from '../../assets/projects/sable/first-steps.webp';
 import UnityImg from '../../assets/projects/sable/unity.webp';
 import Arrow from '../../assets/projects/sable/arrow.svg';
@@ -27,14 +29,22 @@ const Mango = ({mouseEnter, mouseExit}) => {
 			textColor={"#FF7701"} 
 			color={"#001C65"} 
 			categories={["Video Games", "Web", "AI"]} 
-			description={"A cyber & coding website I created for my students to practice computational skills. With advanced lore, leaderboards, skins, gifts, users, cool graphics, music & animations, the website offers a fun and educational experience."}
+			description={"A cyber & coding website I created for my students to practice their computational skills. With advanced lore, leaderboards, skins, gifts, users, cool graphics, music & animations, the website offers a fun and educational experience."}
 			mouseEnter={mouseEnter} 
 			mouseExit={mouseExit} >
 			<div className="Mango">
 				<div className="development" >
-					<h4 className="title">Challenges</h4>
-					<div className='main' style={{backgroundImage: `url("${InGame}")`}} alt="" />
-					<h6 className='responsible'>I'm responsible for designing the product and developing the game's client and server side.</h6>
+					<h4 className="title">The process</h4>
+					<div className='main' style={{backgroundImage: `url("${Banner}")`}} alt="" />
+					<h6 className='responsible'>The website was made using the brand new Next.js 13 and firebase as the database for the players and challenges. The core was built in 3 days, and the whole process of making the features took a month.</h6>
+					<h6 className='responsible'>Video Example</h6>
+					<YouTube iframeClassName="ifr" videoId='3Q0Vygp0k4Y' title='EXAMPLE' opts={
+						{
+							playerVars: {
+								// https://developers.google.com/youtube/player_parameters
+								autoplay: 1,	
+							},
+						}} />
 				</div>
 				<div className="graphic">
 					<h4 className="title">Graphic Design</h4>
